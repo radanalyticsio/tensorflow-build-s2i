89b7773ee1d2b5456b36648d7fd925f0620865fc
Subin M <subinm@apache.org>
2018-07-13 18:08:14 -0400

    add code for naming

--- build_tools/utils.py
+++ build_tools/utils.py
@@ -11,7 +11,7 @@ with open(sys.argv[1], 'r') as f:
 #print(doc)
 print("Tensorflow tag", tf.VERSION,"at source_HEAD=",doc['source_HEAD'], \
         "\nbuilt on :\n", 
-        "OS = ",doc['OS_VER'],"march=",doc['nmarch'], \
+        "OS = ",doc['OS_VER'],"march=",doc['march'], \
         "(kernel ",doc['kernel'],")\n" \
         "with Python-",doc['Python_version'],"",doc['GLIBC_VER']," and ",doc['GCC_VER'], \
         )
