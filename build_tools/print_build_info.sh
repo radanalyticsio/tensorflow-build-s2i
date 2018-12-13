c43008660c6dae415b2b85556ea14a28927162ce
Subin <subin@apache.org>
2018-12-13 10:16:18 -0500

    add yaml back

--- build_tools/print_build_info.sh
+++ build_tools/print_build_info.sh
@@ -224,8 +224,8 @@ echo
 #\"swap_total\": \""${SWAP_TOTAL}"\", 
 #pip install pyyaml  
 rm -fr build_info.*
-#echo -e  $TF_BUILD_INFO | python -c 'import yaml,json,sys;obj=json.load(sys.stdin);yy=yaml.safe_dump(obj, default_flow_style=False)
-#print(yy)' >> build_info.yaml
-
 echo -e $TF_BUILD_INFO >> build_info.json
+echo -e  $TF_BUILD_INFO | python -c 'import yaml,json,sys;obj=json.load(sys.stdin);yy=yaml.safe_dump(obj, default_flow_style=False)
+print(yy)' >> build_info.yaml
+
 
